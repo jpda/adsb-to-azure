3dace705a3205437ade655d7c46b6f8187596b76
jpda <john.dandison@live.com>
2017-08-11 20:42:47 -0400

    initial commit

---
+++ adsb-interpreter/Adsb/Message.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+namespace ADSB.Interpreter.Adsb
+{
+    public class Message
+    {
+        public string RawMessage { get; set; }
+        public string[] RawMessageSplit { get; set; }
+        public virtual string MessageType { get; set; }
+        public string TransmissionType { get; set; }
+        public string SessionId { get; set; }
+        public string AircraftId { get; set; }
+        public string Hexident { get; set; }
+        public string FlightId { get; set; }
+        public DateTime MessageDate { get; set; }
+        public DateTime MessageTime { get; set; }
+        public DateTime LogDate { get; set; }
+        public DateTime LogTime { get; set; }
+        public virtual string Callsign { get; set; }
+        public virtual decimal Altitude { get; set; }
+        public virtual decimal GroundSpeed { get; set; }
+        public virtual string Track { get; set; }
+        public virtual decimal Latitude { get; set; }
+        public virtual decimal Longitude { get; set; }
+        public virtual decimal VerticalRate { get; set; }
+        public virtual string Squawk { get; set; }
+        public virtual bool SquawkChange { get; set; }
+        public virtual bool Emergency { get; set; }
+        public virtual bool SPI { get; set; }
+        public virtual bool IsOnGround { get; set; }
+
+        protected Message(string data)
+        {
+            RawMessage = data;
+            RawMessageSplit = data.Split(',');
+            TransmissionType = GetValue<string>(1);
+            SessionId = GetValue<string>(2);
+            AircraftId = GetValue<string>(3);
+            Hexident = GetValue<string>(4);
+            FlightId = GetValue<string>(5);
+            MessageDate = GetValue(6, x => DateTime.Parse(x));
+            MessageTime = GetValue(7, x => DateTime.Parse(x));
+            LogDate = GetValue(8, x => DateTime.Parse(x));
+            LogTime = GetValue(9, x => DateTime.Parse(x));
+            Callsign = GetValue<string>(10);
+            Altitude = GetValue(11, x => decimal.Parse(x));
+            GroundSpeed = GetValue(12, x => decimal.Parse(x));
+            Track = GetValue<string>(13);
+            Latitude = GetValue(14, x => decimal.Parse(x));
+            Longitude = GetValue(15, x => decimal.Parse(x));
+            VerticalRate = GetValue(16, x => decimal.Parse(x));
+            Squawk = GetValue<string>(17);
+            SquawkChange = GetValue(18, x => x == "1");
+            Emergency = GetValue(19, x => x == "1");
+            SPI = GetValue(20, x => x == "1");
+            IsOnGround = GetValue(21, x => x == "1");
+        }
+
+        public static Message Parse(string data)
+        {
+            var pieces = data.Split(',');
+            var type = int.Parse(pieces[1]);
+
+            switch (type)
+            {
+                case 1: return new IdentificationAndCategoryMessage(data);
+                case 2: return new SurfacePositionMessage(data);
+                case 3: return new AirbornePositionMessage(data);
+                case 4: return new AirborneVelocityMessage(data);
+                case 5: return new SurveillanceAltMessage(data);
+                case 6: return new SurveillanceIdMessage(data);
+                case 7: return new AirToAirMessage(data);
+                case 8: return new AllCallReplyMessage(data);
+                default: return new Message(data);
+            }
+        }
+
+        public T GetValue<T>(int index, Func<string, T> convert = null)
+        {
+            var val = RawMessageSplit[index];
+            if (string.IsNullOrEmpty(val)) return default(T);
+            if (convert == null) return (T)(object)val;
+            try
+            {
+                return convert(val);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return default(T);
+        }
+    }
+
+    public class IdentificationAndCategoryMessage : Message
+    {
+        public override string MessageType { get => 1.ToString(); }
+        public override string Callsign { get; set; }
+
+        public IdentificationAndCategoryMessage(string data) : base(data)
+        {
+            Callsign = GetValue<string>(10);
+        }
+    }
+
+    public class SurfacePositionMessage : Message
+    {
+        public override string MessageType { get => 2.ToString(); }
+        public override decimal Altitude { get => base.Altitude; set => base.Altitude = value; }
+        public override decimal GroundSpeed { get => base.GroundSpeed; set => base.GroundSpeed = value; }
+        public override string Track { get => base.Track; set => base.Track = value; }
+        public override decimal Latitude { get => base.Latitude; set => base.Latitude = value; }
+        public override decimal Longitude { get => base.Longitude; set => base.Longitude = value; }
+        public override bool IsOnGround { get => base.IsOnGround; set => base.IsOnGround = value; }
+
+        public SurfacePositionMessage(string data) : base(data)
+        {
+            var decParse = new Func<string, decimal>(x => decimal.Parse(x));
+            Altitude = GetValue(11, decParse);
+            GroundSpeed = GetValue(12, decParse);
+            Track = GetValue<string>(13);
+            Latitude = GetValue(14, decParse);
+            Longitude = GetValue(15, decParse);
+            IsOnGround = GetValue(21, x => bool.Parse(x));
+        }
+    }
+
+    public class AirbornePositionMessage : Message
+    {
+        public override string MessageType { get => 3.ToString(); }
+        public override decimal Altitude { get => base.Altitude; set => base.Altitude = value; }
+        public override decimal Latitude { get => base.Latitude; set => base.Latitude = value; }
+        public override decimal Longitude { get => base.Longitude; set => base.Longitude = value; }
+        public override bool SquawkChange { get => base.SquawkChange; set => base.SquawkChange = value; }
+        public override bool Emergency { get => base.Emergency; set => base.Emergency = value; }
+        public override bool SPI { get => base.SPI; set => base.SPI = value; }
+        public override bool IsOnGround { get => base.IsOnGround; set => base.IsOnGround = value; }
+
+        public AirbornePositionMessage(string data) : base(data)
+        {
+
+        }
+    }
+
+    public class AirborneVelocityMessage : Message
+    {
+        public override string MessageType { get => 4.ToString(); }
+        public override decimal GroundSpeed { get => base.GroundSpeed; set => base.GroundSpeed = value; }
+        public override string Track { get => base.Track; set => base.Track = value; }
+        public override decimal VerticalRate { get => base.VerticalRate; set => base.VerticalRate = value; }
+
+        public AirborneVelocityMessage(string data) : base(data)
+        {
+
+        }
+    }
+
+    public class SurveillanceAltMessage : Message
+    {
+        public override string MessageType { get => 5.ToString(); }
+        public override decimal Altitude { get => base.Altitude; set => base.Altitude = value; }
+        public override bool SquawkChange { get => base.SquawkChange; set => base.SquawkChange = value; }
+        public override bool SPI { get => base.SPI; set => base.SPI = value; }
+        public override bool IsOnGround { get => base.IsOnGround; set => base.IsOnGround = value; }
+
+        public SurveillanceAltMessage(string data) : base(data)
+        {
+
+        }
+    }
+
+    public class SurveillanceIdMessage : Message
+    {
+        public override string MessageType { get => 6.ToString(); }
+        public override decimal Altitude { get => base.Altitude; set => base.Altitude = value; }
+        public override decimal GroundSpeed { get => base.GroundSpeed; set => base.GroundSpeed = value; }
+        public override string Squawk { get => base.Squawk; set => base.Squawk = value; }
+        public override bool SquawkChange { get => base.SquawkChange; set => base.SquawkChange = value; }
+        public override bool Emergency { get => base.Emergency; set => base.Emergency = value; }
+        public override bool SPI { get => base.SPI; set => base.SPI = value; }
+        public override bool IsOnGround { get => base.IsOnGround; set => base.IsOnGround = value; }
+
+        public SurveillanceIdMessage(string data) : base(data) { }
+    }
+
+    public class AirToAirMessage : Message
+    {
+        public override string MessageType { get => 7.ToString(); }
+        public override decimal Altitude { get => base.Altitude; set => base.Altitude = value; }
+        public override decimal GroundSpeed { get => base.GroundSpeed; set => base.GroundSpeed = value; }
+        public override bool IsOnGround { get => base.IsOnGround; set => base.IsOnGround = value; }
+
+        public AirToAirMessage(string data) : base(data) { }
+    }
+
+    public class AllCallReplyMessage : Message
+    {
+        public override string MessageType { get => 8.ToString(); }
+        public override bool IsOnGround { get => base.IsOnGround; set => base.IsOnGround = value; }
+
+        public AllCallReplyMessage(string data) : base(data) { }
+    }
+}
--- adsb-interpreter/Program.cs
+++ adsb-interpreter/Program.cs
@@ -1,12 +1,163 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace adsb_interpreter
+namespace ADSB.Interpreter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var c = new Connector("192.168.17.113", 30003);
+        }
+    }
+
+    public class Connector
+    {
+        private readonly string _ip;
+        private readonly int _port;
+        private TcpClient _client;
+
+        private long SessionId;
+
+        List<Adsb.Message> _things = new List<Adsb.Message>();
+
+        public Connector(string IP, int port)
+        {
+            _ip = IP;
+            _port = port;
+            Start(IP, port).Wait();
+        }
+
+        public async Task Start(string server, int port)
+        {
+            do
+            {
+                var connected = await Connect(server, port);
+                if (connected)
+                {
+                    SessionId = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds * 100;
+                    //Connected?.Invoke(this, new NewSessionEventArgs(SessionId.ToString()));
+                    await ReadStream();
+                }
+                await Task.Delay(10000);
+            } while (true);
+        }
+
+        private async Task<bool> Connect(string server, int port)
+        {
+            try
+            {
+                _client = new TcpClient();// server, port);
+                await _client.ConnectAsync(server, port);
+                //var prop = new Dictionary<string, string>() { { "Server", server }, { "Port", port.ToString() } };
+                //Log.Event("TcpClient-Connected", prop);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log.Error(ex);
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+
+        private async Task ReadStream()
+        {
+            var data = new byte[256];
+            var sbuffer = new StringBuilder(256);
+            var mb = new MessageBuffer();
+            var contCount = 0;
+
+            try
+            {
+                while (_client.Connected)
+                {
+                    var stream = _client.GetStream();
+                    if (!stream.DataAvailable)
+                    {
+                        //If data is not available in roughly 15 seconds.. disconnect
+                        if (contCount > 15)
+                        {
+                            EndSession();
+                            return;
+                        }
+
+                        await Task.Delay(1000);
+                        contCount++;
+                        continue;
+                    }
+
+                    contCount = 0;
+                    var bytes = stream.Read(data, 0, data.Length);
+
+
+                    sbuffer.Clear();
+                    sbuffer.Append(Encoding.ASCII.GetString(data, 0, bytes));
+
+                    //todo: parse this to a real object? maybe if we foresee lots of downstream subscribers in the future
+
+                    mb.AddData(sbuffer.ToString()).ForEach(m =>
+                    {
+                        sbuffer.Clear().Append(m.Trim());
+                        //todo: this is going to move to Async soon
+                        //Parallel.ForEach(_observers, x =>
+                        //{
+                        //    x.OnNext($"{SessionId.ToString()};{sbuffer.ToString()}");
+                        //});
+                        var message = Adsb.Message.Parse(sbuffer.ToString());
+                        _things.Add(message);
+                        //Console.WriteLine($"Message Type: {message.GetType().Name}, TransmissionType: {message.TransmissionType}");
+                    });
+
+                    if (_things.Count > 10000)
+                    {
+                        EndSession();
+                        var types = _things.GroupBy(x => x.GetType().Name).Select(y => new { Key = y.Key, Count = y.Count() }).OrderByDescending(z => z.Count);
+                        foreach (var t in types)
+                        {
+                            Console.WriteLine($"{t.Key}: {t.Count}");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Log.Error(e);
+                //Log.Event("Stream read failed.");
+                //Log.Event("TcpClient-Connection-Closed");
+                Console.WriteLine(e);
+                EndSession();
+            }
+        }
+
+        private void EndSession()
+        {
+            _client.Dispose();
+        }
+    }
+
+    public class MessageBuffer
+    {
+        private readonly List<string> _buffer = new List<string>();
+
+        public List<string> AddData(string data)
+        {
+            if (_buffer.Any()) { data = $"{_buffer.First()}{data}"; }
+            // <new> 
+            // updated to parse record based on CRLF delimmiter
+            //
+            string[] delimiter = { "\r\n" };
+            var lines = data.Split(delimiter, System.StringSplitOptions.None).Where(x => x.Length > 0).ToList();
+            //var lines = data.Split("$".ToCharArray()).Where(x => x.Trim().Length > 0).Select(y => "$" + y).ToList();
+            _buffer.Clear();
+            var carryoverLine = lines.Last();
+            _buffer.Add(carryoverLine);
+            lines.Remove(carryoverLine);
+            return lines;
         }
     }
 }
